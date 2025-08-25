a78e2cbd8de7fa4b72773fdee88edcd105aa7040
yashika <yashikasah362@gmail.com>
2025-08-25 17:07:34 +0530

    changes in route

--- app/api/generateFlyer/route.js
+++ app/api/generateFlyer/route.js
@@ -3,10 +3,11 @@ import axios from "axios";
 const API_BASE_URL = "http://localhost:5000"; // backend ka URL
 
 // flyer generate karne ke liye
-export const generateFlyer = async (prompt) => {
+export const generateFlyer = async (prompt,inputImageUrl) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/api/generateFlyer`, {
       prompt,
+      inputImageUrl
     });
     return response.data; // { flyerUrl: "..."}
   } catch (error) {
